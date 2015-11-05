c37202aa5bec486491e995c5c49612d377cb2529
Nick Patavalis <npat@efault.net>
2015-11-05 10:26:33 +0200

    termios: Added unsupported.go
    Build only this file on totally unsupported (non-Unix) systems.

---
+++ termios/unsupported.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2015, Nick Patavalis (npat@efault.net).
+// All rights reserved.
+// Use of this source code is governed by a BSD-style license that can
+// be found in the LICENSE file.
+
+// +build !linux,!freebsd,!netbsd,!openbsd,!darwin,!dragonfly,!solaris
+
+// Package termios is a simple Go wrapper to the POSIX Terminal
+// Interface (POSIX Termios).
+//
+// This package is not supported on your system.
+package termios
